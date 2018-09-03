be62461815c5859dd2f07c7051079471ed2fd327
krishnas2 <krishnakanth1432@gmail.com>
2018-09-03 18:38:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded());
 
 // parse application/json
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.post('/details', (req, res,next) =>{  
