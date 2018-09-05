083ade80c032219f67e3d9500e6ad40d91c664a6
krishnas2 <krishnakanth1432@gmail.com>
2018-09-05 15:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,26 @@ app.use(bodyParser.json());
     next();
 });*/
 
-
+var dataops=(data)=>{
+//console.log('databv bvbv          ',data);
+		//console.log(data['entities']['object'][0]['value'],data['entities']['operation'][0]['value'],data['entities']['info'][0]['value']);
+		var temp={},vals=[];
+		temp['object']=data['entities']['object'][0]['value'];
+		temp['operation']=data['entities']['operation'][0]['value'];
+		temp['raw_result']=data;
+		var loc=data['entities']['info'][0]['value'].split(',');
+		for(var i=0;i<loc.length;i++){
+			if (i==0)
+			{
+				loc[i]=loc[i].replace(/(with|having|With|Having)/i, '').trim();
+			}
+			loc[i]=loc[i].replace(/( as)/i, '').trim();
+			vals=loc[i].split(' ');
+			temp[vals[0]]=vals[1];
+		}
+		//console.log('temp is      ' ,temp);
+		return temp;
+	};
 app.post('/details', (req, res,next) =>{  
 console.log('req is ',req.body.query);
 temp='';
@@ -30,7 +49,7 @@ temp='';
 	//logger: new log.Logger(log.DEBUG) // optional
 	client.message(req.body.query).then((data) => {
 	  console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
-	  res.send(JSON.stringify(data['entities']));
+	  res.send(dataops(JSON.stringify(data)));
 			res.end();
 	});
 	
